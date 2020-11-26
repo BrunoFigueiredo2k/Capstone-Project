8b39c6aa337be984b94be81be0013d341f996495
@BrunoFigueiredo2k <brunofigueiredo@live.nl>
2020-11-26 21:00:50 +0100

    Add some basic stuff like colors, strings and fragments

--- app/src/main/java/com/example/capstone_project/ui/fragments/MoviesFragment.kt
+++ app/src/main/java/com/example/capstone_project/ui/fragments/MoviesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.capstone_project.ui
+package com.example.capstone_project.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,7 +12,7 @@ import com.example.capstone_project.R
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class MoviesFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/com/example/capstone_project/ui/fragments/TabsFragment.kt
+++ app/src/main/java/com/example/capstone_project/ui/fragments/TabsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.capstone_project.ui
+package com.example.capstone_project.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -12,7 +12,7 @@ import com.example.capstone_project.R
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class TabsFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
